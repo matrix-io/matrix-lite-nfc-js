a6fcb8bdae85887ca6879a91272e8465543f3cc5
Hermitter <chacin.carlos9798@gmail.com>
2019-09-04 18:39:34 -0400

    Started JS abstraction

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
+const nfc = require('./build/Release/addon');
+
+const reader = require('./lib/reader.js');
+
+module.exports = {
+    // Returns a string based on number given (used for NFC status codes)
+    'status': nfc.status,
+    'reader': reader,
+};
+
 // UNDER DEVELOPMENT * NOTHING FINAL
 // The code below is a working async read/write example while
 // the library is being finished.
 
-var nfc = require('./build/Release/addon');
-var matrix = require('@matrix-io/matrix-lite');
+// var nfc = require('./build/Release/addon');
+// var matrix = require('@matrix-io/matrix-lite');
 
-console.log(nfc);
+// console.log(nfc);
 
-// // Testing C++ object wrapping
-var message = new nfc.ndefParser();
-message.addUriRecord("tel:+14085551212"); // https://developer.apple.com/documentation/corenfc/adding_support_for_background_tag_reading
-message.addTextRecord("Hello2222");
+// // // Testing C++ object wrapping
+// var message = new nfc.ndefParser();
+// message.addUriRecord("tel:+14085551212"); // https://developer.apple.com/documentation/corenfc/adding_support_for_background_tag_reading
+// message.addTextRecord("Hello2222");
 
-console.log("THE NEW RECORD!");
-console.log(message.getRecords());
-console.log(message.getRecord(0));
+// console.log("THE NEW RECORD!");
+// console.log(message.getRecords());
+// console.log(message.getRecord(0));
 
 
 // message.getRecord();
@@ -39,22 +49,3 @@ console.log(message.getRecord(0));
 
 //     }, {info: true});// you can remove what you don't want to read
 // }, 1000);
-
-nfc.read((code, tag)=>{
-        // Read tag
-        if(code === 256){
-            // write.ndef(message);
-            var message2 = new nfc.ndefParser(tag.ndef.content);
-            console.log(message2.records());
-            // var text = new nfc.ndefParser(tag.ndef.content);
-            // console.log(text.toString());
-            matrix.led.set({g:1});
-        }
-
-        // Didn't read tag
-        else if (code === 1024){
-            console.log("no tag found")
-            matrix.led.set();
-        }
-
-    }, {ndef: true});// you can remove what you don't want to read
