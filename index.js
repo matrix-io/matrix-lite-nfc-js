bbb1ff57f4deecf1eb523336f9b15b6447e86c58
Hermitter <chacin.carlos9798@gmail.com>
2019-09-05 01:56:40 -0400

    Updated read interval

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const nfc = require('./build/Release/addon');
 
 const read = require('./lib/read.js');
+const write = require('./lib/write.js');
 
 module.exports = {
     // Returns a string based on number given (used for NFC status codes)
     'status': nfc.status,
     'read': read,
+    'write': write,
+    'message': nfc.ndefParser
 };
 
 // UNDER DEVELOPMENT * NOTHING FINAL
